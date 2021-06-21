5f174a1d9962d7362bae333d11e9a906f50aef94
Innocent Ncube <68056743+InnocentNcube@users.noreply.github.com>
2021-06-21 17:05:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
-class Worker:
-
-    def __init__(self, name, age, city):
-        self.name = "Kasia"
-        self.age = age
+from string import Template
 
 
-worker = Worker("Anna", 23, "Warsaw")
-print(worker.name)
-print(worker.city)
+class MyTemplate(Template):
+    delimiter = '#'
+
+
+def Main():
+    cart = []
+    cart.append(dict(item="Fanta", price=9, qty=3))
+    cart.append(dict(item="Pizza", price=61, qty=2))
+    cart.append(dict(item="Chocolate", price=14, qty=2))
+    cart.append(dict(item="Cola", price=3, qty=1))
+    cart.append(dict(item="Water", price=2, qty=1))
+    cart.append(dict(item="Laptop", price=2000, qty=1))
+    cart.append(dict(item="Smartphone", price=800, qty=1))
+    cart.append(dict(item="Airpods", price=650, qty=2))
+    cart.append(dict(item="Sneakers", price=469, qty=1))
+    cart.append(dict(item="suit", price=1400, qty=2))
+
+    t = MyTemplate("#qty x #item = #price")
+    total = 0
+    print("Cart:")
+    for data in cart:
+        print(t.substitute(data))
+        total += data["price"]
+    print("Total: " + str(total))
+
+
+if __name__ == '__main__':
+    Main()
